597a44a7f38b509357a3ad21255a682451298d1e
blackram <blackram@live.com.au>
2022-08-29 12:51:49 +0930

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8mwkfu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8mwkfu)
---
+++ src/app/dialog-animations-example.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+ 
+/**
+ * @title Dialog Animations
+ */
+@Component({
+  selector: 'dialog-animations-example',
+  styleUrls: ['dialog-animations-example.css'],
+  templateUrl: 'dialog-animations-example.html',
+})
+export class DialogAnimationsExample {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.html',
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) {}
+}
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
